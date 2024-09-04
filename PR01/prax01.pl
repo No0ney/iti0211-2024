9cd51a2cc0a91475e7f1843003e564409d1cf693
ameier <anna.meier0211@gmail.com>
2024-09-04 16:56:01 +0300

    prax01

---
+++ PR01/prax01.pl
@@ -0,0 +1,46 @@
+mother(emma, jane).
+mother(carol, jane).
+mother(harry, jane).
+
+mother(jake, emma).
+mother(lily, emma).
+
+mother(kate, carol).
+mother(simon, carol).
+
+mother(may, megan).
+
+mother(gary, may).
+mother(mary, may).
+
+married(jane, bob).
+married(emma, alan).
+married(carol, oliver).
+married(megan, rob).
+married(may, harry).
+
+male(bob).
+male(rob).
+male(harry).
+male(alan).
+male(oliver).
+male(jake).
+male(simon).
+male(gary).
+
+female(jane).
+female(carol).
+female(emma).
+female(megan).
+female(may).
+female(lily).
+female(kate).
+female(mary).
+
+father(Child, Father):- married(Wife, Father), mother(Child, Wife).
+brother(Child, Brother):- mother(Child, Mom), male(Brother), mother(Brother, Mom).
+sister(Child, Sister):- mother(Child, Mom), female(Sister), mother(Sister, Mom).
+aunt(Child, Aunt):- mother(Child, Mom), sister(mom, Aunt).
+uncle(Child, Uncle):- mother(Child, Mom), brother(Mom, Uncle).
+grandfather(Child, Grandfather):- mother(Child, Mom), mother(Mom, Grandma), married(Grandma, Grandfather).
+grandmother(Child, Grandmother):- mother(Child, Mom), mother(Mom, Grandmother).
