c91e02f35dcec27d2bfa569c2eb10e5853d425ad
ameier <anna.meier0211@gmail.com>
2024-09-10 16:04:47 +0300

    prax01.1

--- PR01/prax01.pl
+++ PR01/prax01.pl
@@ -38,8 +38,8 @@ female(kate).
 female(mary).
 
 father(Child, Father):- married(Wife, Father), mother(Child, Wife).
-brother(Child, Brother):- mother(Child, Mom), male(Brother), mother(Brother, Mom).
-sister(Child, Sister):- mother(Child, Mom), female(Sister), mother(Sister, Mom).
+brother(Child, Brother):- Child /= Brother, mother(Child, Mom), male(Brother), mother(Brother, Mom).
+sister(Child, Sister):- Child /= Sister, mother(Child, Mom), female(Sister), mother(Sister, Mom).
 aunt(Child, Aunt):- mother(Child, Mom), sister(mom, Aunt).
 uncle(Child, Uncle):- mother(Child, Mom), brother(Mom, Uncle).
 grandfather(Child, Grandfather):- mother(Child, Mom), mother(Mom, Grandma), married(Grandma, Grandfather).
