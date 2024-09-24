c2d84c2fef937196b8575ce272b1f1c45da85f79
ameier <anna.meier0211@gmail.com>
2024-09-24 17:04:51 +0300

    push 6

--- PR03/prax03.pl
+++ PR03/prax03.pl
@@ -7,8 +7,8 @@ suurim([El], [El]).
 
 paki([], []).
 paki([El], [El]).
-paki([El1, El2 | Tail], [_|Y]):- El1 \= El2, paki([El2 | Tail], [El1 | Y]).
-paki([El1, El2 | Tail], [X|Y]):- paki([El2 | Tail], [X|Y]).
+paki([El1, El2 | Tail], [El1|Y]):- El1 \= El2, paki([El2 | Tail], Y).
+paki([_, El2 | Tail], X):- paki([El2 | Tail], X).
 
 duplikeeri([], []).
 duplikeeri([El], [El]).
@@ -16,4 +16,11 @@ duplikeeri([El1, El2 | Tail], [X|Y]):- duplikeeri([El2 | Tail], [El1, El1 | Y]).
 
 kordista([], X, []).
 
-vordle_predikaadiga([], [], []).
+paaritu_vordle([El], )
+paaritu_vordle([El1, El2 | Tail], [X|Y]):- paaritu_vordle([_ | Tail], [El1, Y]).
+paaris_vordle([El1, El2 | Tail], [X|Y]).
+suurem_vordle([El1 | Tail], N, [X|Y]).
+vordle_predikaadiga(List, [Pred | N], X):-
+    (Pred = paaritu_arv, paaritu_vordle(List, X)) ;
+    (Pred = paaris_arv, paaris_vordle(List, X)) ;
+    (Pred = suurem_kui(X), suurem_vordle(List, N, X)).
