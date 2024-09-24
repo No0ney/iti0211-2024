861703fca825b568719510b26854b1e76e3fb2cb
ameier <anna.meier0211@gmail.com>
2024-09-24 15:50:27 +0300

    push 3

--- PR03/prax03.pl
+++ PR03/prax03.pl
@@ -1,2 +1,10 @@
 viimane_element(Last, [Last|[]]).
 viimane_element(Last, [_|Tail]):- viimane_element(X, [Tail]).
+
+suurim([], []).
+suurim([Head|[]], [X|Y]):- append(Head, Y, X).
+suurim([Head, _ | []], [X|Y]):- append(Head, Y, X).
+suurim([Head1, Head2 | Tail], [X|Y]):-
+((Head1 > Head2, append(Head1, Y, X)) ; (Head1 < Head2, append(Head2, Y, X))), suurim(Tail, X).
+
+%paki([Head | Tail], [X|Y]):- .
