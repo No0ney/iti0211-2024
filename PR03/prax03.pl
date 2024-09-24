922fef9aab487535fc7977cc77dbc28ec999468f
ameier <anna.meier0211@gmail.com>
2024-09-24 16:38:10 +0300

    push 5

--- PR03/prax03.pl
+++ PR03/prax03.pl
@@ -2,10 +2,18 @@ viimane_element(Last, [Last|[]]).
 viimane_element(Last, [_|Tail]):- viimane_element(X, [Tail]).
 
 suurim([], []).
-suurim([X], [X]).
+suurim([El], [El]).
 %suurim([Head1, Head2 | Tail], [X|Y]):- .
 
 paki([], []).
-paki([X], [X]).
-paki([El1, El2 | Tail], [X|Y]):- El1 \= El2, paki([El2 | Tail], [El1 | Y]).
+paki([El], [El]).
+paki([El1, El2 | Tail], [_|Y]):- El1 \= El2, paki([El2 | Tail], [El1 | Y]).
 paki([El1, El2 | Tail], [X|Y]):- paki([El2 | Tail], [X|Y]).
+
+duplikeeri([], []).
+duplikeeri([El], [El]).
+duplikeeri([El1, El2 | Tail], [X|Y]):- duplikeeri([El2 | Tail], [El1, El1 | Y]).
+
+kordista([], X, []).
+
+vordle_predikaadiga([], [], []).
