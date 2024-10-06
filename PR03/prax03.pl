eaa2e18de84c115b5ca0b2682abe098b1eb0beb6
ameier <anna.meier0211@gmail.com>
2024-10-06 18:58:37 +0300

    push 20

--- PR03/prax03.pl
+++ PR03/prax03.pl
@@ -23,29 +23,29 @@ kordista([], _, []).
 kordista(L, 1, L).
 kordista([El1 | Tail], N, X):- append(L, P, X), add_el(El1, N, L), kordista(Tail, N, P).
 
-paaritu_vordle([], _, _).
-paaritu_vordle([El | Tail], List, X):-
+paaritu_arv([], _, _).
+paaritu_arv([El | Tail], List, X):-
     (mod(El, 2) =\= 0, append(List, [El], New),
-    ((Tail = [], X = New) ; paaritu_vordle(Tail, New, X))) ;
+    ((Tail = [], X = New) ; paaritu_arv(Tail, New, X))) ;
     (mod(El, 2) =:= 0,
-    ((Tail = [], X = List) ; paaritu_vordle(Tail, List, X))).
+    ((Tail = [], X = List) ; paaritu_arv(Tail, List, X))).
 
-paaris_vordle([], _, _).
-paaris_vordle([El | Tail], List, X):-
+paaris_arv([], _, _).
+paaris_arv([El | Tail], List, X):-
     (mod(El, 2) =:= 0, append(List, [El], New),
-    ((Tail = [], X = New) ; paaris_vordle(Tail, New, X))) ;
+    ((Tail = [], X = New) ; paaris_arv(Tail, New, X))) ;
     (mod(El, 2) =\= 0,
-    ((Tail = [], X = List) ; paaris_vordle(Tail, List, X))).
+    ((Tail = [], X = List) ; paaris_arv(Tail, List, X))).
 
-suurem_vordle([], _, _, _).
-suurem_vordle([El | Tail], N, List, X):-
+suurem_kui([], _, _, _).
+suurem_kui([El | Tail], N, List, X):-
     (El > N, append(List, [El], New),
-    ((Tail = [], X = New) ; suurem_vordle(Tail, N, New, X))) ;
+    ((Tail = [], X = New) ; suurem_kui(Tail, N, New, X))) ;
     ((El < N ; El =:= N),
-    ((Tail = [], X = List) ; suurem_vordle(Tail, N, List, X))).
+    ((Tail = [], X = List) ; suurem_kui(Tail, N, List, X))).
 
 vordle_predikaadiga([], _, []).
 vordle_predikaadiga(List, [Pred | N], X):-
-    (Pred == "paaritu_arv", paaritu_vordle(List, [], X)) ;
-    (Pred == "paaris_arv", paaris_vordle(List, [], X)) ;
-    (Pred == "suurem_kui", suurem_vordle(List, N, [], X)).
+    (Pred == "paaritu_arv", paaritu_arv(List, [], X)) ;
+    (Pred == "paaris_arv", paaris_arv(List, [], X)) ;
+    (Pred == "suurem_kui", suurem_kui(List, N, [], X)).
