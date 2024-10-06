b7ddc721bb01ffc703c440e2f73d75899c266b47
ameier <anna.meier0211@gmail.com>
2024-10-06 20:24:53 +0300

    push 1

---
+++ PR04/prax04.pl
@@ -0,0 +1,23 @@
+laevaga(tallinn, helsinki, 120).
+laevaga(tallinn, stockholm, 480).
+
+bussiga(tallinn, riia, 300).
+
+rongiga(riia, berlin, 680).
+
+lennukiga(tallinn, helsinki, 30).
+lennukiga(helsinki, paris, 180).
+lennukiga(paris, berlin, 120).
+lennukiga(paris, tallinn, 120 ).
+
+reisi(From, To):-
+    laevaga(From, To, _) ;
+    bussiga(From, To, _) ;
+    rongiga(From, To, _) ;
+    lennukiga(From, To, _).
+reisi(From, To):-
+    (laevaga(From, Between, _) ;
+    bussiga(From, Between, _) ;
+    rongiga(From, Between, _) ;
+    lennukiga(From, Between, _)), reisi(Between, To).
+
