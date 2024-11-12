354391e2bf2c7b7b9c24bb4266a798b42e928823
ameier <anna.meier0211@gmail.com>
2024-11-12 23:33:16 +0200

    push 5

--- PR07/prax07.pl
+++ PR07/prax07.pl
@@ -17,16 +17,21 @@ eats(veeimetajad,kalad).
 
 :- dynamic lastNode/1.
 
-count_terminals(Node, Terminals, Count):-
-    is_a(X, Node),
-    count_terminals(X, Terminals, Count),
-    count_terminals(Terminals, Count).
-count_terminals(Node, _, _):-
-    not(is_a(_, Node)),
-    assert(lastNode(Node)),
-    count_terminals(Terminals, Count).
-count_terminals(Terminals, Count):-
-    findall(Node, lastNode(Node), Terminals),
-    length(Terminals, Count).
+count_terminals(Node, Terminals, Count):- true.
+
+extintion(Who,What_spieces,How_many):- true.
+
+find_most_sensitive_species(Died, Count, Species):- true.
+
+%    is_a(X, Node),
+%    count_terminals(X, Terminals, Count),
+%    count_terminals(Terminals, Count).
+%count_terminals(Node, _, _):-
+%    not(is_a(_, Node)),
+%    assert(lastNode(Node)),
+%    count_terminals(Terminals, Count).
+%count_terminals(Terminals, Count):-
+%    findall(Node, lastNode(Node), Terminals),
+%    length(Terminals, Count).
 
 %extinction(Who,What_spieces,How_many):-.
