8d2843c6f6bdecb835ada204bb4eddae74695ab2
ameier <anna.meier0211@gmail.com>
2024-11-13 00:30:39 +0200

    push 9

--- PR07/prax07.pl
+++ PR07/prax07.pl
@@ -26,6 +26,20 @@ count_terminals(Node, Terminals, Count):-
     findall(Terminal, find_terminal(Node, Terminal), Terminals),
     length(Terminals, Count).
 
-extintion(Who, What_spieces, How_many):- true.
+extinction(Who, What_spieces, Count) :-
+    findall(Species, will_go_extinct(Who, Species), ExtinctSpecies),
+    will_go_extinct2(ExtinctSpecies, [], What_spieces),
+    length(What_spieces, Count).
+
+will_go_extinct(Who, Who).
+will_go_extinct(Who, Species) :-
+    eats(Predator, Who),
+    will_go_extinct(Predator, Species).
+
+will_go_extinct2([], List, List).
+will_go_extinct2([El | Tail], List, What_spieces):-
+    findall(Terminal, find_terminal(El, Terminal), Terminals),
+    append(List, Terminals, NewList),
+    will_go_extinct2(Tail, NewList, What_spieces).
 
 find_most_sensitive_species(Died, Count, Species):- true.
