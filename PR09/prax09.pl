5c88d66f24882ecc27facef6900d5a63c566a13b
ameier <anna.meier0211@gmail.com>
2024-11-19 15:32:00 +0200

    push 1

---
+++ PR09/prax09.pl
@@ -0,0 +1,18 @@
+liitlause --> lihtlause, liitlause.
+liitlause --> lihtlause.
+
+lihtlause --> nimisonafraas, tegusonafraas.
+
+nimisonafraas --> nimisona.
+nimisonafraas --> omadussonafraas, nimisona.
+
+omadussonafraas --> omadussona, nimisona.
+
+tegusonafraas --> tegusona.
+tegusonafraas --> tegusona, sidusona.
+tegusonafraas --> tegusona, nimisona.
+
+nimisona --> [kivile]; [sammal]; [uhkus]; [upakile]; [raha]; [volad].
+omadussona --> [veerevale].
+tegusona --> [ei, kasva]; [ajab]; [tuleb]; [laheb]; [jaavad].
+sidusona --> [','].
