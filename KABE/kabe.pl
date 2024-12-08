69b8c7edb0834d42489d14750c39b9f2d233fbf8
ameier <anna.meier0211@gmail.com>
2024-12-08 23:09:21 +0200

    push 3

--- KABE/kabe.pl
+++ KABE/kabe.pl
@@ -2,7 +2,7 @@
 
 do_turn(Color, X, Y):-
     possible_moves(Color, X, Y, List),
-    choose_best_move(List, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], Move),
+    choose_best_move(List, [0, 0, 0, 0, 0, 0], Move),
     do_move(Move),
     !.
 do_turn(_, _, _).
@@ -27,13 +27,19 @@ other_color(2, 10).
 other_color(20, 1).
 other_color(20, 10).
 
+dame_color(1, 10).
+dame_color(2, 20).
+
 within_boarder(X):- member(X, [1, 2, 3, 4, 5, 6, 7, 8]).
 
 
 
 possible_moves(Color, 0, 0, List):-
     find_direction(Color, Direction),
-    findall(ruut(X, Y, Color), ruut(X, Y, Color), Roots),
+    dame_color(Color, DameColor),
+    findall(ruut(X, Y, Color), ruut(X, Y, Color), MenRoots),
+    findall(ruut(X, Y, DameColor), ruut(X, Y, DameColor), DameRoots),
+    append(MenRoots, DameRoots, Roots),
     check_if_can_take_piece(Roots, Direction, TakeList),
     (length(TakeList, Len),
     (Len \= 0, append([], TakeList, List))
@@ -48,22 +54,16 @@ possible_moves(Color, X, Y, List):-
     length(TakeList, Len),
     (Len \= 0, append([], TakeList, List)) ; append([], MoveList, List).
 
-%move_options(X, Y, Color, Direction, TakeList, MoveList):-
-%    check_if_can_take_piece(X, Y, Color, Direction, TakeList),
-%    check_change_position(X, Y, Color, Direction, MoveList).
-
 check_if_can_take_piece([], _, []).
 check_if_can_take_piece([ruut(X, Y, Color) | Roots], Direction, TakeList):-
     (member(Color, [1, 2]) ->
-    findall([X, Y, X1, Y1, X2, Y2, X3, Y3, X4, Y4],
-    (moves_for_men_take(X, Y, Direction, X1, Y1, X2, Y2),
-    check_for_men_take_again(X2, Y2, X1, Y1, Direction, X3, Y3, X4, Y4)),
+    findall([X, Y, X1, Y1, X2, Y2],
+    moves_for_men_take(X, Y, Direction, X1, Y1, X2, Y2),
     TakeList1)
     ;
     (member(Color, [10, 20]) ->
-    findall([X, Y, X1, Y1, X2, Y2, X3, Y3, X4, Y4],
-    (moves_for_dames_take(X, Y, X1, Y1, X2, Y2),
-    check_for_dames_take_again(X2, Y2, X1, Y1, X3, Y3, X4, Y4)),
+    findall([X, Y, X1, Y1, X2, Y2],
+    moves_for_dames_take(X, Y, X1, Y1, X2, Y2),
     TakeList1))),
     check_if_can_take_piece(Roots, Direction, TakeList2),
     append(TakeList1, TakeList2, TakeList).
@@ -71,10 +71,10 @@ check_if_can_take_piece([ruut(X, Y, Color) | Roots], Direction, TakeList):-
 check_change_position([], _, []).
 check_change_position([ruut(X, Y, Color) | Roots], Direction, MoveList):-
     (member(Color, [1, 2]) ->
-    findall([X, Y, 0, 0, X1, Y1, 0, 0, 0, 0], moves_for_men_move(X, Y, Direction, X1, Y1), MoveList1)
+    findall([X, Y, 0, 0, X1, Y1], moves_for_men_move(X, Y, Direction, X1, Y1), MoveList1)
     ;
     (member(Color, [10, 20]) ->
-    findall([X, Y, 0, 0, X1, Y1, 0, 0, 0, 0], moves_for_dames_move(X, Y, X1, Y1), MoveList1))),
+    findall([X, Y, 0, 0, X1, Y1], moves_for_dames_move(X, Y, X1, Y1), MoveList1))),
     check_change_position(Roots, Direction, MoveList2),
     append(MoveList1, MoveList2, MoveList).
 
@@ -198,11 +198,11 @@ moves_for_dames_take(X, Y, X1, Y1, X2, Y2):-
     check_no_ally_between(X, Y, Color, Spaces - 1, X1, Y1),
     ruut(X1, Y1, OtherColor),
     Color =\= OtherColor, OtherColor =\= 0,
-    member(AfterSpaces, [1, 2, 3, 4, 5, 6, 7]),
-    X2 is X1 + AfterSpaces, within_boarder(X2),
-    Y2 is Y1 - AfterSpaces, within_boarder(Y2),
-    ruut(X2, Y2, 0),
-    check_no_ally_between(X1, Y1, Color, AfterSpaces - 1, X2, Y2).
+%    member(AfterSpaces, [1, 2, 3, 4, 5, 6, 7]),
+    X2 is X1 + 1, within_boarder(X2),
+    Y2 is Y1 - 1, within_boarder(Y2),
+    ruut(X2, Y2, 0).
+%    check_no_ally_between(X1, Y1, Color, AfterSpaces - 1, X2, Y2).
 
 % Check up-right
 moves_for_dames_take(X, Y, X1, Y1, X2, Y2):-
@@ -213,11 +213,11 @@ moves_for_dames_take(X, Y, X1, Y1, X2, Y2):-
     check_no_ally_between(X, Y, Color, Spaces - 1, X1, Y1),
     ruut(X1, Y1, OtherColor),
     Color =\= OtherColor, OtherColor =\= 0,
-    member(AfterSpaces, [1, 2, 3, 4, 5, 6, 7]),
-    X2 is X1 + AfterSpaces, within_boarder(X2),
-    Y2 is Y1 + AfterSpaces, within_boarder(Y2),
-    ruut(X2, Y2, 0),
-    check_no_ally_between(X1, Y1, Color, AfterSpaces - 1, X2, Y2).
+%    member(AfterSpaces, [1, 2, 3, 4, 5, 6, 7]),
+    X2 is X1 + 1, within_boarder(X2),
+    Y2 is Y1 + 1, within_boarder(Y2),
+    ruut(X2, Y2, 0).
+%    check_no_ally_between(X1, Y1, Color, AfterSpaces - 1, X2, Y2).
 
 % Check down-left
 moves_for_dames_take(X, Y, X1, Y1, X2, Y2):-
@@ -228,11 +228,11 @@ moves_for_dames_take(X, Y, X1, Y1, X2, Y2):-
     check_no_ally_between(X, Y, Color, Spaces - 1, X1, Y1),
     ruut(X1, Y1, OtherColor),
     Color =\= OtherColor, OtherColor =\= 0,
-    member(AfterSpaces, [1, 2, 3, 4, 5, 6, 7]),
-    X2 is X1 - AfterSpaces, within_boarder(X2),
-    Y2 is Y1 - AfterSpaces, within_boarder(Y2),
-    ruut(X2, Y2, 0),
-    check_no_ally_between(X1, Y1, Color, AfterSpaces - 1, X2, Y2).
+%    member(AfterSpaces, [1, 2, 3, 4, 5, 6, 7]),
+    X2 is X1 - 1, within_boarder(X2),
+    Y2 is Y1 - 1, within_boarder(Y2),
+    ruut(X2, Y2, 0).
+%    check_no_ally_between(X1, Y1, Color, AfterSpaces - 1, X2, Y2).
 
 % Check down-right
 moves_for_dames_take(X, Y, X1, Y1, X2, Y2):-
@@ -243,11 +243,11 @@ moves_for_dames_take(X, Y, X1, Y1, X2, Y2):-
     check_no_ally_between(X, Y, Color, Spaces - 1, X1, Y1),
     ruut(X1, Y1, OtherColor),
     Color =\= OtherColor, OtherColor =\= 0,
-    member(AfterSpaces, [1, 2, 3, 4, 5, 6, 7]),
-    X2 is X1 - AfterSpaces, within_boarder(X2),
-    Y2 is Y1 + AfterSpaces, within_boarder(Y2),
-    ruut(X2, Y2, 0),
-    check_no_ally_between(X1, Y1, Color, AfterSpaces - 1, X2, Y2).
+%    member(AfterSpaces, [1, 2, 3, 4, 5, 6, 7]),
+    X2 is X1 - 1, within_boarder(X2),
+    Y2 is Y1 + 1, within_boarder(Y2),
+    ruut(X2, Y2, 0).
+%    check_no_ally_between(X1, Y1, Color, AfterSpaces - 1, X2, Y2).
 
 check_for_dames_take_again(X2, Y2, X1, Y1, X3, Y3, X4, Y4):-
     (moves_for_dames_take(X2, Y2, X3, Y3, X4, Y4), X3 \= X1, Y3 \= Y1)
@@ -283,29 +283,25 @@ moves_for_dames_move(X, Y, X1, Y1):-
 % ===========================================================================
 
 choose_best_move([], Move, Move).
-choose_best_move([[X, Y, X1, Y1, X2, Y2, X3, Y3, X4, Y4] | List],
-                 [N, M, N1, M1, N2, M2, N3, M3, N4, M4], Move):-
+choose_best_move([[X, Y, X1, Y1, X2, Y2] | List],
+                 [N, M, N1, M1, N2, M2], Move):-
     ruut(X, Y, Color),
     ((Color = 1 -> DameSpace is 8) ; (Color = 2 -> DameSpace is 1) ; true),
-    ((N4 = 0, X4 \= 0 ; N2 = 0, X2 \= 0 ; (N2 = 0 ; N2 \= 0), X2 = DameSpace ; N = 0, X \= 0) ->
-    append([], [X, Y, X1, Y1, X2, Y2, X3, Y3, X4, Y4], Best)
+    ((N2 = 0, X2 \= 0 ; (N2 = 0 ; N2 \= 0), X2 = DameSpace ; N = 0, X \= 0) ->
+    append([], [X, Y, X1, Y1, X2, Y2], Best)
     ;
-    append([], [N, M, N1, M1, N2, M2, N3, M3, N4, M4], Best)),
+    append([], [N, M, N1, M1, N2, M2], Best)),
     choose_best_move(List, Best, Move).
 
 % ===========================================================================
 % Do the best move
 % ===========================================================================
 
-do_move([X, Y, X1, Y1, X2, Y2, X3, Y3, X4, Y4]):-
+do_move([X, Y, X1, Y1, X2, Y2]):-
     ruut(X, Y, Color),
     retract(ruut(X, Y, Color)),
     assertz(ruut(X, Y, 0)),
     (X1 \= 0 -> retract(ruut(X1, Y1, _)), assertz(ruut(X1, Y1, 0)) ; true),
     retract(ruut(X2, Y2, 0)),
     assertz(ruut(X2, Y2, Color)),
-    check_dame(X2, Y2, Color),
-    (X3 \= 0 -> retract(ruut(X3, Y3, _)), assertz(ruut(X3, Y3, 0)) ; true),
-    (X4 \= 0 -> retract(ruut(X2, Y2, _)), assertz(ruut(X2, Y2, 0)),
-                retract(ruut(X4, Y4, _)), assertz(ruut(X4, Y4, Color)),
-                check_dame(X4, Y4, Color) ; true).
+    check_dame(X2, Y2, Color).
